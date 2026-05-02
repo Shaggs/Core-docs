e1c5f2ed408f1742640e8f5742bb0d8180fb1b0a
Shane Rees <rees.shane@gmail.com>
2026-05-02 20:41:40 +0930

    Delete add_admin.py

--- add_admin.py
+++
@@ -1,9 +0,0 @@
-from app import app, db, User
-
-with app.app_context():
-    u = User(email="admin@locals")
-    u.set_password("Admin1234")
-    u.is_super_admin = True
-    u.must_change_password = False
-    db.session.add(u)
-    db.session.commit()
