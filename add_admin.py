81f3262ac0e1f08d2439104994347143a83073fc
Shane Rees <rees.shane@gmail.com>
2026-05-02 20:38:14 +0930

    Change admin user email and password

--- add_admin.py
+++ add_admin.py
@@ -1,8 +1,8 @@
 from app import app, db, User
 
 with app.app_context():
-    u = User(email="shane@locals")
-    u.set_password("Alarm1001")
+    u = User(email="admin@locals")
+    u.set_password("Admin1234")
     u.is_super_admin = True
     u.must_change_password = False
     db.session.add(u)
